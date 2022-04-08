955a7943e92762df50d668ba02e1c187e64c08d1
sabademetrashvili1 <97677640+sabademetrashvili1@users.noreply.github.com>
2022-04-09 03:03:16 +0400

    Update base64.py

--- base64.py
+++ base64.py
@@ -1,17 +1,25 @@
 import base64
 
-sample_string = input("enter string: ")
-sample_string_bytes = sample_string.encode("ascii")
+def base64_encode():
+    string = input("Enter string: ")
+    string_bytes = string.encode("ascii")
 
-base64_bytes = base64.b64encode(sample_string_bytes)
-base64_string = base64_bytes.decode("ascii")
 
-print(f"Encoded string: {base64_string}")
+    base64_bytes = base64.b64encode(string_bytes)
+    base64_string = base64_bytes.decode("ascii")
 
-base64_string = input("enter string: ")
-base64_bytes = base64_string.encode("ascii")
+    print("Encoded string: %s" % base64_string)
 
-sample_string_bytes = base64.b64decode(base64_bytes)
-sample_string = sample_string_bytes.decode("ascii")
 
-print(f"Decoded string: {sample_string}")
+base64_encode()
+
+def base64_decode():
+    base64_string = input("Enter decoded string: ")
+    base64_bytes = base64_string.encode('ascii')
+
+    string = base64.b64decode(base64_bytes)
+    string_bytes = string.decode('ascii')
+
+    print("Decoded string: %s" % string_bytes)
+
+base64_decode()
